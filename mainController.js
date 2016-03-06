42ad50add9aaf2256787c33d1d8f0ab477c0693e
LKHohn <lkhohn@gmail.com>
2016-03-06 16:24:17 -0700

    angular anchorscroll and responsive layout

--- mainController.js
+++ mainController.js
@@ -1,2 +1,21 @@
-angular.module("app")
-  .controller("MainController", [MainController]);
+  angular.module('app', [])
+  .controller('MainController', ['$scope', '$location', '$anchorScroll', MainController ]);
+
+  function MainController($scope, $location, $anchorScroll){
+    var vm = this;
+    vm.scrollAbout = scrollAbout;
+    vm.scrollProjects = scrollProjects;
+    vm.scrollResume = scrollResume;
+    function scrollAbout(){
+      $location.hash('aboutSection');
+      $anchorScroll();
+    }
+    function scrollProjects(){
+      $location.hash('projectScrollSection');
+      $anchorScroll();
+    }
+    function scrollResume(){
+      $location.hash('resumeScrollSection');
+      $anchorScroll();
+    }
+  }
